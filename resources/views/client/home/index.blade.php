7e89a1c176db6fdcd9c332e54f47fd079d95b785
OtterGJLS <ariffjul43@gmail.com>
2025-08-26 09:16:51 +0700

    Progress8.1

--- resources/views/client/home/index.blade.php
+++ resources/views/client/home/index.blade.php
@@ -478,7 +478,7 @@
                     }
                 }).mount();
             });
-            // Kode untuk Animasi On-Scroll
+            // Animasi On-Scroll
             const observer = new IntersectionObserver((entries) => {
                 entries.forEach((entry) => {
                     if (entry.isIntersecting) {
@@ -486,7 +486,7 @@
                     }
                 });
             }, {
-                threshold: 0.1 // Elemen dianggap terlihat jika 10% areanya masuk layar
+                threshold: 0.1 
             });
 
             const elementsToAnimate = document.querySelectorAll('.animate-on-scroll');
